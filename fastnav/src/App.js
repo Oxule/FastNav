6ee0bbd8b190c2dd8317b31f842a087142972152
Oxule <ribb2017@mail.ru>
2024-11-07 21:40:47 +0300

    added main project & page structure

--- fastnav/src/App.js
+++ fastnav/src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div>
+import {useAnimatedText} from "./useAnimatedText";
 
-    </div>
-  );
+export default function App() {
+    const [target, setTarget] = useAnimatedText("/home/oxule/test/");
+
+    return (
+        <>
+            <div className={"mt-[150px]"}>
+                <div className={"text-5xl text-center mb-[50px]"}>
+                    Доберитесь до <div className={"inline text-accent"}>{target}</div>
+                </div>
+                <div className={"text-4xl text-center"}>/home/<input className={"bg-inputbg rounded-br-2xl rounded-tr-2xl"}/></div>
+            </div>
+        </>
+      );
 }
-
-export default App;
--- fastnav/tailwind.config.js
+++ fastnav/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      desktop: '768px',
+    },
+    colors: {
+      'accent': '#2c76c9',
+      'inputbg': '#1d2733',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
